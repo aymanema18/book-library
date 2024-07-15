11af915be2cbda9b4f9549c7e8d6bf2e3e4c6bdd
aymanema18 <aymanema18@gmail.com>
2024-07-15 18:50:39 +0100

    Write script for form validation

--- script.js
+++ script.js
@@ -12,82 +12,125 @@ let dialog = document.querySelector(".form-dialog");
 let tBody = document.querySelector(".t-body");
 let addBtn = document.querySelector(".add-btn");
 let deleteBtn = document.querySelectorAll(".delete-btn");
+const tM = document.querySelector(".title-message");
+const aM = document.querySelector(".author-message");
+const pM = document.querySelector(".pages-message");
 
 let i = 0;
 
+titleInput.addEventListener("blur", () => {
+  if (titleInput.validity.valueMissing) {
+    tM.textContent = "Must not be left empty.";
+  }
+});
+
+titleInput.addEventListener("focus", () => {
+  tM.textContent = "";
+});
+
+authorInput.addEventListener("blur", () => {
+  if (authorInput.validity.valueMissing) {
+    aM.textContent = "Must not be left empty.";
+  }
+});
+
+authorInput.addEventListener("focus", () => {
+  aM.textContent = "";
+});
+
+pagesInput.addEventListener("blur", () => {
+  if (pagesInput.validity.valueMissing) {
+    pM.textContent = "Must not be left empty.";
+  } else if (pagesInput.validity.rangeUnderflow) {
+    pM.textContent = "Pages must be over 0";
+  }
+});
+
+pagesInput.addEventListener("focus", () => {
+  pM.textContent = "";
+});
 
 class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-    changeStatus() {
-        if (this.read === "read") {
-            this.read = "not read";
-        } else {
-            this.read = "read";
-        }
+  changeStatus() {
+    if (this.read === "read") {
+      this.read = "not read";
+    } else {
+      this.read = "read";
     }
+  }
 }
 
-
 openBtn.addEventListener("click", () => {
-    dialog.showModal();
-})
+  dialog.showModal();
+});
 
 closeBtn.addEventListener("click", () => {
-    dialog.close();
-})
+  dialog.close();
+});
 
 addBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    if (!titleInput.value 
-        || !authorInput.value 
-        || !pagesInput.value
-        || !rInput.checked && !nrInput.checked) {
-        //
-    } else {
-        if (rInput.checked) {
-            checkedBtn = rInput;
-        } else if (nrInput.checked) {
-            checkedBtn = nrInput;
-        }
-        dialog.close();
-        library.push(new Book(titleInput.value, authorInput.value, pagesInput.value, checkedBtn.value));
-        tBody.innerHTML += `<tr data-index="${i}">
+  event.preventDefault();
+  if (
+    !titleInput.value ||
+    !authorInput.value ||
+    !pagesInput.value ||
+    pagesInput.validity.rangeUnderflow ||
+    (!rInput.checked && !nrInput.checked)
+  ) {
+    //
+  } else {
+    if (rInput.checked) {
+      checkedBtn = rInput;
+    } else if (nrInput.checked) {
+      checkedBtn = nrInput;
+    }
+    dialog.close();
+    library.push(
+      new Book(
+        titleInput.value,
+        authorInput.value,
+        pagesInput.value,
+        checkedBtn.value
+      )
+    );
+    tBody.innerHTML += `<tr data-index="${i}">
         <td data-index="${i}">${titleInput.value}</td>
         <td data-index="${i}">${authorInput.value}</td>
         <td data-index="${i}">${pagesInput.value}</td>
         <td data-index="${i}" class="read-btn">${checkedBtn.value}</td>
         <td data-index="${i}"><div data-index="${i}" class="delete-btn">DELETE</div></td>
     </tr>`;
-        i++;
-        titleInput.value = "";
-        authorInput.value = "";
-        pagesInput.value = "";
-        checkedBtn.checked = false;
-    }
-    deleteBtn = document.querySelectorAll(".delete-btn");
-})
+    i++;
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    checkedBtn.checked = false;
+  }
+  deleteBtn = document.querySelectorAll(".delete-btn");
+});
 
 tBody.addEventListener("click", (event) => {
-    if (event.target.classList.contains("delete-btn")) {
-      let deleteIndex = event.target.dataset.index;
-      delete library[deleteIndex];
-      document.querySelector(`tr[data-index="${deleteIndex}"]`).remove();
-    }
-  })
+  if (event.target.classList.contains("delete-btn")) {
+    let deleteIndex = event.target.dataset.index;
+    delete library[deleteIndex];
+    document.querySelector(`tr[data-index="${deleteIndex}"]`).remove();
+  }
+});
 
 tBody.addEventListener("click", (event) => {
-    if (event.target.classList.contains("read-btn")) {
-        library[event.target.dataset.index].changeStatus();
-        if (event.target.innerHTML === "read") {
-            event.target.innerHTML = "not read";
-        } else {
-            event.target.innerHTML = "read";
-        }
+  if (event.target.classList.contains("read-btn")) {
+    library[event.target.dataset.index].changeStatus();
+    if (event.target.innerHTML === "read") {
+      event.target.innerHTML = "not read";
+    } else {
+      event.target.innerHTML = "read";
     }
-})
+  }
+});
